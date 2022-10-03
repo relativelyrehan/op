b89dabf851900ec1eed123aa1535caa4acf4bad8
relativelyrehan <rehan18alam@gmail.com>
2022-10-03 17:38:42 +0530

    xx

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function detectBrowser(e) {
+function detectBrowser(e, u) {
   if (!e) return "unable to detect";
+  if (!u) return "invalid url";
   let i = e.toLowerCase().trim(),
     r = i?.includes("android"),
     n = i.includes("iphone") || i.includes("ipod") || i.includes("ipad"),
     t = i?.includes("macintosh"),
     o = i?.includes("win");
 
-  const link =
-    "intent://www.relativelyrehan.co/terminal#Intent;scheme=http;action=android.intent.action.VIEW;end;";
+  const link = `intent://${u}#Intent;scheme=http;action=android.intent.action.VIEW;end;`;
   for (let s of [
     "facebook",
     "fb",
@@ -23,14 +23,15 @@ function detectBrowser(e) {
         return JSON.stringify({ browser: s, OS: "Android" });
       }
       if (n) {
-        window.location.replace(
-          "googlechromes://" + "relativelyrehan.co/terminal"
-        );
+        window.location.replace("googlechromes://" + u);
         return JSON.stringify({ browser: s, OS: "iOS" });
       }
     }
 }
-const u = detectBrowser(window.navigator.userAgent);
+const u = detectBrowser(
+  window.navigator.userAgent,
+  window.location.href.split("https://")[1]
+);
 const oa = {
   getBrowser: () => u,
 };
