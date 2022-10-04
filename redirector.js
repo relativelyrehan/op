a9d76ad3a5a3864d54c8a9661188d06cc5541230
relativelyrehan <rehan18alam@gmail.com>
2022-10-04 16:07:48 +0530

    xx

--- redirector.js
+++ redirector.js
@@ -1,31 +1,49 @@
 function h(w) {
+  var _w$location, _w$navigator;
+
   if (!w) return;
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-
   var raw = JSON.stringify({
-    source_url: w?.location?.href,
+    source_url:
+      w === null || w === void 0
+        ? void 0
+        : (_w$location = w.location) === null || _w$location === void 0
+        ? void 0
+        : _w$location.href,
     user_id: 11231,
-    user_agent: w?.navigator?.userAgent,
+    user_agent:
+      w === null || w === void 0
+        ? void 0
+        : (_w$navigator = w.navigator) === null || _w$navigator === void 0
+        ? void 0
+        : _w$navigator.userAgent,
   });
-
   var requestOptions = {
     method: "POST",
     headers: myHeaders,
     body: raw,
     redirect: "follow",
   };
-
   fetch("https://service.openinapp.com/api/v1/link/generate", requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-      if (result?.data?.smart_link && result?.data?.inApp) {
-        window.location.href = "https://" + result?.data?.smart_link;
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (result) {
+      if (
+        result &&
+        result.data &&
+        result.data.smart_link &&
+        result.data.inApp
+      ) {
+        window.location.href = "https://" + result.data.smart_link;
       } else {
         return "native";
       }
     })
-    .catch((error) => console.log("error", error));
+    .catch(function (err) {
+      console.log(err);
+    });
 }
 
 h(window);
