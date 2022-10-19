8e15c6e1d98a6852b7dce6be87c42b2aa0584af5
relativelyrehan <rehan18alam@gmail.com>
2022-10-19 19:28:01 +0530

    xx

--- simple.js
+++ simple.js
@@ -40,7 +40,7 @@ function h(w) {
     })
     .then(function (result) {
       if (result && result.data && result.data.url != "") {
-        alert(result.data.url.replace("http://", ""));
+        // alert(result.data.url.replace("http://", ""));
         window.location.href = result.data.url.replace("http://", "");
       } else {
         return;
