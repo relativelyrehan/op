47e315c8a25e55b1e53f41d8b94c53f39eb3b4a2
relativelyrehan <rehan18alam@gmail.com>
2022-10-19 19:36:38 +0530

    xx

--- hide.js
+++ hide.js
@@ -1,7 +1,7 @@
+document.addEventListener("DOMContentLoaded", i, false);
 function i() {
   document.body.style.display = "none";
   setTimeout(() => {
     document.body.style.display = "block";
   }, 200);
 }
-i();
